833cfa684da6ac8f6a0d794495eea4353c2a98be
Lars De Pauw <depauwlars@gmail.com>
2015-05-26 11:14:46 +0200

    starte

---
+++ src/main/java/Book.java
@@ -0,0 +1,56 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Book implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Book() {};
+
+    public Book(Integer id, String title, String author, String isbn) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+insert into book(title, author, isbn) values('Nineteen Eighty
+             Four', 'George Orwell', '0000000001');
+insert into book(title, author, isbn) values('Alice''s Adventures
+             In Wonderland', 'Lewis Carroll', '0000000002');
+insert into book(title, author, isbn) values('A brief history of time',
+            'Stephen Hawking', '0000000003');
+insert into book(title, author, isbn) values('They live!',
+            'Jean Idea', '0000000004');
+insert into book(title, author, isbn) values('King Arthur and the round table',
+            'MacleHeim', '0000000005');
+insert into book(title, author, isbn) values('De alchemist', 'George Paldwin', '0000000006');
