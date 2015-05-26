9574ce330669ec962450a2e35cfaa20ae1b1821f
Lars De Pauw <depauwlars@gmail.com>
2015-05-26 13:41:02 +0200

    update

--- src/main/java/TestEntityManager.java
+++ src/main/java/TestEntityManager.java
@@ -1,23 +1,30 @@
+
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
 
+
 import static org.junit.Assert.*;
 
 public class TestEntityManager {
     private EntityManager em;
+    private Logger logger = LoggerFactory.getLogger(TestEntityManager.class);
+
 
     @Test
     public void testOurLogic() {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("RealDolmenPersistenceUnit");
         EntityManager em = entityManagerFactory.createEntityManager();
         em.getTransaction().begin();
+
         BookRepository bookRepository = new BookRepositoryBean(em);
         List<Book> books = bookRepository.findAllBooks();
-        assertEquals(6, books.size());
+        assertEquals(7, books.size());
 
         em.getTransaction().commit();
         em.close();
@@ -25,9 +32,8 @@ public class TestEntityManager {
 
 
     }
-
-
 }
+
 
 
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,4 @@ insert into book(title, author, isbn) values('A brief history of time','Stephen 
 insert into book(title, author, isbn) values('They live!', 'Jean Idea', '0000000004');
 insert into book(title, author, isbn) values('King Arthur and the round table','MacleHeim', '0000000005');
 insert into book(title, author, isbn) values('De alchemist', 'George Paldwin', '0000000006');
+insert into book(title, author, isbn) values('Java for and by dummies', 'Lars De Pauw', '0000000007');
